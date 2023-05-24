394544c41086f001e0bcaa47137ee5533e44b3a2
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-24 08:47:31 -0300

    create global styles of application

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    backgroundColor: '$blue_100',
+    color: '$white_100',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+  }
+})
---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import { createStitches } from '@stitches/react'
+
+export const { config, createTheme, css, getCssText, globalCss, keyframes, styled, theme } = createStitches({
+  theme: {
+    colors: {
+      blue_100: '#1d3557',
+      blue_200: '#457b9d',
+      blue_300: '#a8dadc',
+
+      white_100: '#f1faee',
+
+      red_100: '#e63946',
+
+    }
+  }
+})
