b764b469a729dbb14d0f6173f5254c34470cce1c
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-24 09:46:40 -0300

    create style page app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
+import { Body, Container, Header, LogoImage } from "../styles/pages/app"
+import logoImg from "../assets/ball.svg"
+import Image from "next/image"
 
 globalStyles()
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-      <Component {...pageProps} />
+      <Container>
+        <Body>
+          <Header>
+            <h1>Meu Time</h1>
+            <LogoImage>
+              <Image src={logoImg} width={50} height={50} alt="logo"/>
+            </LogoImage>
+          </Header>
+          <Component {...pageProps} />
+        </Body>
+      </Container>
     )
 }
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,46 @@
+import { styled, keyframes } from ".."
+
+const isRotating = keyframes({
+  to: {
+    transform: 'rotate(1turn)',
+  },
+})
+
+export const Container = styled('div', {
+  display: 'flex',
+})
+
+export const Body = styled('div', {
+  margin: '2rem',
+  padding: '2rem',
+  width: '100%',
+  height: 'calc(100vh - 4rem)',
+  backgroundColor: '$blue_300',
+  borderRadius: '5px',
+})
+
+export const Header = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  backgroundColor: '$blue_200',
+  padding: '0.5rem 2rem',
+  borderRadius: '10px',
+  boxShadow: '4px 4px 5px rgba(0, 0, 0, 0.5)',
+
+  'h1': {
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+  }
+})
+
+export const LogoImage = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  animationName: `${isRotating}`,
+  animationDirection: 'normal',
+  animationDuration: '3s',
+  animationTimingFunction: 'linear',
+  animationIterationCount: 'infinite'
+})
